710a367434e20946e5fcb28d6ed375cd1ab81ab2
elhoucinesmaili <elhoucinesmaili51@gmail.com>
2025-05-13 19:24:19 +0000

    commit

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,31 +1,29 @@
 #!/usr/bin/python3
 """
-Validation UTF-8
+UTF-8 Validation1
 """
+
 
 def validUTF8(data):
     """
-    data : liste d'entiers représentant des octets
-    Retourne : True si les données représentent une
-    encodage UTF-8 valide, sinon False
+    data: a list of integers
+    Return: True if data is a valid UTF-8
+    encoding, else return False
     """
-    byte_count = 0  # Nombre d'octets restants à valider pour un caractère multioctets
+    byte_count = 0
 
     for i in data:
         if byte_count == 0:
-            # Déterminer le nombre d'octets dans le caractère UTF-8 en cours
-            if i >> 5 == 0b110:
-                byte_count = 1  # Caractère sur 2 octets
+            if i >> 5 == 0b110 or i >> 5 == 0b1110:
+                byte_count = 1
             elif i >> 4 == 0b1110:
-                byte_count = 2  # Caractère sur 3 octets
+                byte_count = 2
             elif i >> 3 == 0b11110:
-                byte_count = 3  # Caractère sur 4 octets
+                byte_count = 3
             elif i >> 7 == 0b1:
-                return False  # Caractère invalide (octet de départ incorrect)
+                return False
         else:
-            # Vérifier que l'octet suivant commence par '10'
             if i >> 6 != 0b10:
                 return False
             byte_count -= 1
-
-    return byte_count == 0  # Retourne True uniquement si tous_
+    return byte_count == 0
