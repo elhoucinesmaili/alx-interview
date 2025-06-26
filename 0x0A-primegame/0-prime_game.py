e039c848e0b54746afa420ed05219c376b53216d
elhoucinesmaili <elhoucinesmaili51@gmail.com>
2025-06-26 23:08:26 +0000

    up

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,42 +1,43 @@
 #!/usr/bin/python3
+"""Prime Game module"""
 
-def sieve_of_eratosthenes(n):
-    """Return a list where index i is True if i is prime."""
-    sieve = [True] * (n + 1)
-    sieve[0:2] = [False, False]  # 0 and 1 are not prime
-    for i in range(2, int(n ** 0.5) + 1):
-        if sieve[i]:
-            for multiple in range(i * i, n + 1, i):
-                sieve[multiple] = False
-    return sieve
 
 def isWinner(x, nums):
+    """Determines the winner of the prime game.
+    """
     if not nums or x < 1:
         return None
 
     max_n = max(nums)
-    is_prime = sieve_of_eratosthenes(max_n)
-
-    # Precompute the number of primes up to each n
-    primes_count = [0] * (max_n + 1)
-    count = 0
-    for i in range(1, max_n + 1):
-        if is_prime[i]:
-            count += 1
-        primes_count[i] = count
-
-    maria_wins = 0
-    ben_wins = 0
+    primes = sieve_of_eratosthenes(max_n)
+    wins = {"Maria": 0, "Ben": 0}
 
     for n in nums:
-        if primes_count[n] % 2 == 1:
-            maria_wins += 1
+        prime_count = count_primes_up_to(n, primes)
+        if prime_count % 2 == 0:
+            wins["Ben"] += 1
         else:
-            ben_wins += 1
+            wins["Maria"] += 1
 
-    if maria_wins > ben_wins:
+    if wins["Maria"] > wins["Ben"]:
         return "Maria"
-    elif ben_wins > maria_wins:
+    elif wins["Ben"] > wins["Maria"]:
         return "Ben"
     else:
         return None
+
+
+def sieve_of_eratosthenes(n):
+    """Generates a list indicating prime status up to n
+    using Sieve of Eratosthenes"""
+    is_prime = [False, False] + [True] * (n - 1)
+    for i in range(2, int(n**0.5) + 1):
+        if is_prime[i]:
+            for j in range(i * i, n + 1, i):
+                is_prime[j] = False
+    return is_prime
+
+
+def count_primes_up_to(n, primes):
+    """Counts how many prime numbers are ≤ n using precomputed sieve"""
+    return sum(primes[0:n + 1])
